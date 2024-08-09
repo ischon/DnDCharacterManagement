559f6ec4886e79b5cfb75e9bb40fba14034c14a7
Ian Schön <ian@florbs.io>
2024-08-10 00:37:21 +0200

    chore: update code

--- src/models/Examples.js
+++ src/models/Examples.js
@@ -34,12 +34,12 @@ exampleCharacter.bonds = "" +
 exampleCharacter.flaws = "" +
     "Most people scream and run when they see a demon. I stop and take notes on its anatomy."
 // character information
-exampleCharacter.class = "Artificer"
+exampleCharacter.class = "Artificer" // // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
 exampleCharacter.level = 2
-exampleCharacter.race = "Rock Gnome"
-exampleCharacter.background = "Sage"
+exampleCharacter.race = "Rock Gnome" // https://www.dndbeyond.com/races/18-gnome
+exampleCharacter.background = "Sage" // https://www.dndbeyond.com/backgrounds/11-sage
 exampleCharacter.alignment = "Chaotic good"
-exampleCharacter.experiencePoints = "nvt"
+exampleCharacter.experiencePoints = "-"
 exampleCharacter.addLanguage("Common") // https://www.dndbeyond.com/races/18-gnome#GnomeTraits// https://forgottenrealms.fandom.com/wiki/Common
 exampleCharacter.addLanguage("Gnomish (Dwarvish script)") // https://www.dndbeyond.com/races/18-gnome#GnomeTraits // https://forgottenrealms.fandom.com/wiki/Gnome_language
 exampleCharacter.addLanguage("Modron") // study // https://www.dndbeyond.com/backgrounds/11-sage // https://forgottenrealms.fandom.com/wiki/Modron_language
@@ -49,9 +49,9 @@ exampleCharacter.addFeature("Gnome Cunning") // https://www.dndbeyond.com/races/
 exampleCharacter.addFeature("Artificer's Lore") // https://www.dndbeyond.com/races/18-gnome#RockGnome
 exampleCharacter.addFeature("Tinker") // https://www.dndbeyond.com/races/18-gnome#RockGnome
 exampleCharacter.addFeature("Researcher")
-exampleCharacter.addFeature("Magical Tinkering")
+exampleCharacter.addFeature("Magical Tinkering") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
 exampleCharacter.addFeature(
-    "Infuse Item (lv2)\\n" +
+    "Infuse Item (lv2)\\n" + // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
     "* enchanted arcane focus\\n" +
     "* Repeating shot\\n" +
     "* Replicate magical item (bag of holding)\\n" +
@@ -68,13 +68,16 @@ exampleCharacter.charisma = 10
 exampleCharacter.proficiencyBonus = 2
 exampleCharacter.inspiration = 0
 exampleCharacter.proficiencyBonus = 2
-exampleCharacter.addProficiency("dexterity", "Sleight of Hand")
-exampleCharacter.addProficiency("constitution", "Saving Throws")
-exampleCharacter.addProficiency("intelligence", "Saving Throws")
 exampleCharacter.addProficiency("intelligence", "Arcana") // https://www.dndbeyond.com/backgrounds/11-sage
 exampleCharacter.addProficiency("intelligence", "History") // https://www.dndbeyond.com/backgrounds/11-sage
-exampleCharacter.addProficiency("intelligence", "Investigation")
-exampleCharacter.addProficiency("items", "artisan’s tools (tinker’s tools)")
+exampleCharacter.addProficiency("constitution", "Saving Throws") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("intelligence", "Saving Throws") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("intelligence", "Investigation") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("dexterity", "Sleight of Hand")  // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("items", "Armor: Light armor, medium armor, shields") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("items", "Weapons: Simple weapons") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("items", "Tools: Thieves' tools, tinker’s tools, one type of artisan’s tools of your choice") // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addProficiency("items", "Artisan’s tools (tinker’s tools)") // https://www.dndbeyond.com/races/18-gnome#RockGnome
 exampleCharacter.armorClassBase = 14
 exampleCharacter.armorClassHasDexModifier = true
 exampleCharacter.speed = 25
@@ -85,9 +88,9 @@ exampleCharacter.hitDice = "2D8"
 exampleCharacter.currentHitDice = 2
 exampleCharacter.deathSaveSuccesses = 0
 exampleCharacter.deathSaveFailures = 0
-exampleCharacter.addAttack("light Crossbow", 4, "1d8", "Piercing")
-exampleCharacter.addAttack("Dagger", 4, "1d4", "Piercing")
-exampleCharacter.addAttack("Quarterstaff", 0, "1d6 (1d8)", "Bludgeoning")
+exampleCharacter.addAttack("light Crossbow", 4, "1d8", "Piercing") // https://roll20.net/compendium/dnd5e/Light%20Crossbow#content
+exampleCharacter.addAttack("Dagger", 4, "1d4", "Piercing") // https://roll20.net/compendium/dnd5e/Items:Dagger/#h-Dagger
+exampleCharacter.addAttack("Quarterstaff", 0, "1d6 (1d8)", "Bludgeoning") //https://roll20.net/compendium/dnd5e/Items:Quarterstaff/#h-Quarterstaff
 exampleCharacter.addCantrip("Mending")
 exampleCharacter.addCantrip("Guidance")
 exampleCharacter.addSpell(1, "Tasha's caustic brew")
@@ -99,18 +102,18 @@ exampleCharacter.addPreparedSpell(1, "Absorb Element")
 exampleCharacter.addSpell(1, "Feather fall")
 exampleCharacter.addPreparedSpell(1, "Feather fall")
 exampleCharacter.addSpellSlot(1, 2)
-exampleCharacter.addEquipment("Light Crossbow", 1, 5)
-exampleCharacter.addEquipment("Bolts", 19, 0.075)
-exampleCharacter.addEquipment("Dagger", 1, 1)
-exampleCharacter.addEquipment("Quarterstaff", 1, 4)
-exampleCharacter.addEquipment("Scale Mail", 1, 45)
-exampleCharacter.addEquipment("Thieves' Tools", 1, 1)
+exampleCharacter.addEquipment("Light Crossbow", 1, 5) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addEquipment("Bolts", 19, 0.075) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addEquipment("Dagger", 1, 1) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addEquipment("Quarterstaff", 1, 4) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addEquipment("Scale Mail", 1, 45) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
+exampleCharacter.addEquipment("Thieves' Tools", 1, 1) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
 exampleCharacter.addEquipment("- small file", 1, 0)
 exampleCharacter.addEquipment("- set of lock picks", 1, 0)
 exampleCharacter.addEquipment("- small mirror", 1, 0)
 exampleCharacter.addEquipment("- set of narrow-bladed scissors", 1, 0)
 exampleCharacter.addEquipment("- pair of pliers", 1, 0)
-exampleCharacter.addEquipment("Dungeoneer's Pack", 1, 0)
+exampleCharacter.addEquipment("Dungeoneer's Pack", 1, 0) // https://drive.google.com/file/d/1QVK2-rkohWcVEocqfJSrUD59ipcG_Y-Q
 exampleCharacter.addEquipment("- Backpack", 1, 5)
 exampleCharacter.addEquipment("- Crowbar", 1, 5)
 exampleCharacter.addEquipment("- Hammer", 1, 3)
