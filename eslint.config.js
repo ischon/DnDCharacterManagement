fafb4bf7f8dc1b2b9afab148c951313228e86d7e
Ian Schön <ian@florbs.io>
2025-08-25 16:36:10 +0200

    chore: fix linter

--- eslint.config.js
+++ eslint.config.js
@@ -58,8 +58,8 @@ export default [
     plugins: {
       vue,
     },
+    processor: vue.processors['.vue'],
     rules: {
-      ...vue.configs['vue3-recommended'].rules,
       'vue/multi-word-component-names': 'off',
       'vue/no-v-html': 'off',
       'vue/require-default-prop': 'off',
